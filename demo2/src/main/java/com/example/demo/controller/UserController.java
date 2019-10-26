ef414e00b3d639e1d0e889e72e55c62d2d1d9ae4
TC <chengtian372@gmail.com>
2019-10-26 15:58:33 -0400

    create user

--- demo2/src/main/java/com/example/demo/controller/UserController.java
+++ demo2/src/main/java/com/example/demo/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dao.UserDao;
+import com.example.demo.dao.impl.UserDaoImpl;
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 @RestController
@@ -15,7 +18,10 @@ public class UserController {
 	
 	 @Autowired
 	 UserRepository userRepository;
-	
+	 
+	 @Autowired
+		private UserDaoImpl userdao;
+	 
 	 @GetMapping
 	 public String toTest() {
 	  return "Welcome to Java Inspires...";
@@ -26,4 +32,15 @@ public class UserController {
 	 public List<String> getAllUserNames() {
 	  return userRepository.getAllUserNames();
 	 }
+	 
+	 @GetMapping(path = "/createuser")
+	 private void createUser() 
+	 {
+		User user = new User();
+		user.setUserEmail("xinq1@gmai.com");
+		user.setPassword("123");
+		user.setUserName("qxh");
+		userdao.createUser(user);
+	 }
+		 
 }
