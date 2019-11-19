8f447260dbb5a6cea2698297a5570b9f6dfa08ca
chinmliu <chinmliu@126.com>
2019-11-19 16:38:51 -0500

    new item test

--- AutoBill/app/src/main/java/com/example/autobill/MainActivity.java
+++ AutoBill/app/src/main/java/com/example/autobill/MainActivity.java
@@ -4,19 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.autobill.model.User;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mSu;
-    private Button mBtnFlink, mBtnGlink, mBtnTlink;
+    private Button mSu;
+    private Button mBtnSignIn;
+    private EditText username;
+    private EditText pwd;
+    private String UserName,Pwd;
+    private String is;
+    private Button mBtnFp;
+    private Button mTb;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mSu = findViewById(R.id.su);
         mSu.setOnClickListener(new View.OnClickListener() {
@@ -28,31 +56,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mBtnFlink = findViewById(R.id.f_link);
-        mBtnFlink.setOnClickListener(new View.OnClickListener() {
+
+        username = findViewById(R.id.username1);
+        pwd = findViewById(R.id.password);
+        mBtnSignIn = findViewById(R.id.si);
+        //检查用户名密码，如果正确，则成功登录。
+        mBtnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                Intent intent = new Intent(MainActivity.this,SignedIn.class);
+                startActivity(intent);
+                //UserName = username.getText().toString().trim();
+                //Pwd = pwd.getText().toString().trim();
+                //checkSignin(UserName,Pwd);
+            }
+        });
+
+        mBtnFp = findViewById(R.id.fp);
+        mBtnFp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Testpage.class);
                 startActivity(intent);
             }
         });
 
-        mBtnGlink = findViewById(R.id.g_link);
-        mBtnGlink.setOnClickListener(new View.OnClickListener() {
+        mTb = findViewById(R.id.test);
+        mTb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ProfileSettingsActivity.class);
+                Intent intent = new Intent(MainActivity.this, BillTestActivity.class);
                 startActivity(intent);
             }
         });
 
-        mBtnTlink = findViewById(R.id.t_link);
-        mBtnTlink.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void checkSignin(final String userName,final String pwd){
+        //this function will show the user groups.
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SidesettingActivity.class);
-                startActivity(intent);
-            }
+            public void run() {
+                FormBody body = new FormBody.Builder()
+                        .add("username1",userName)
+                        .add("pwd",pwd)
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://10.0.2.2:8083/login")
+                        .post(body)
+                        .build();
+                OkHttpClient client = new OkHttpClient();
+                Callback callback = new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                    }
+                };
+
+                }
+
         });
     }
+
+
 }
---
+++ AutoBill/app/src/main/java/com/example/autobill/SignedIn.java
@@ -0,0 +1,123 @@
+package com.example.autobill;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class SignedIn extends AppCompatActivity {
+
+
+    private Button mBtnSideView;
+    private Button mBtnFriend;
+    private Button mBtnHistory;
+    private Button test;
+    private TextView result;
+    private RecyclerView recyclerview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+        recyclerview = findViewById(R.id.group_rv);
+        recyclerview.setLayoutManager(new LinearLayoutManager(SignedIn.this));
+        recyclerview.setAdapter(new GroupLinearAdapter(SignedIn.this));
+
+        mBtnSideView = findViewById(R.id.signedin_button);
+        mBtnSideView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignedIn.this,SidesettingActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+//        test = findViewById(R.id.button_6);
+//        result = findViewById(R.id.textview);
+//        test.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                getGroupList();
+//            }
+//        });
+
+
+        mBtnFriend = findViewById(R.id.button_7);
+        mBtnFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignedIn.this,Friends.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnHistory = findViewById(R.id.button_8);
+        mBtnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignedIn.this,History.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void getGroupList(){
+        //this function will show the user groups.
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        final Request request = new Request.Builder()
+                .url("http://10.0.2.2:8085/group/name/3")
+                .get()
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d("Result",e.getMessage());
+            }
+            @Override
+            public void onResponse(@NotNull Call call, final Response response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            result.setText(response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        result.setTextSize(Float.parseFloat("25"));
+                    }
+                });
+
+            }
+        });
+
+    }
+
+
+
+    }
+
+
